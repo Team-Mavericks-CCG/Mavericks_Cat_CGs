5ce8ef9a55920f16dff29aed0d5f227022ffa616
schurchil <schurchill@mail.bradley.edu>
2025-03-28 12:31:37 -0500

    eslint issue in node_modules. it is broken but pushing to share

---
+++ frontend/test-vite/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+//import SignIn from '../../SignInComponent/SignIn'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    
+    
+  </StrictMode>,
+)
