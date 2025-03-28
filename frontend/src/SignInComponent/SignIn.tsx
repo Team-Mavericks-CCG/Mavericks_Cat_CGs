c65c4da4e31e52dabea4bb782e79ccd7bda1dbcc
alsnyder <alsnyder@mail.bradley.edu>
2025-03-28 14:05:54 -0500

    formatting in index.css and signin

--- frontend/src/SignInComponent/SignIn.tsx
+++ frontend/src/SignInComponent/SignIn.tsx
@@ -26,12 +26,12 @@ const Card = styled(MuiCard)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   alignSelf: "center",
-  width: "100%",
+  width: "150%",
   padding: theme.spacing(4),
   gap: theme.spacing(2),
   margin: "auto",
   [theme.breakpoints.up("sm")]: {
-    maxWidth: "450px",
+    maxWidth: "650px",
   },
   boxShadow:
     "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
