7faf1f7fe9689a14068a4c5199498094c5b5ad89
night-0w1-afk <rgodoy@mail.bradley.edu>
2025-04-02 17:53:01 -0500

    updated

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import SignIn from "./SignInComponent/SignIn";
 import { SolitairePage } from "./games/solitaire/solitairePage";
+import Leaderboard from "./leaderboard/leaderboardpage"; 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode> 
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
     <Routes>
         <Route path="/" element={<SignIn />} />  {/* / url is homepage but for now its the signin*/} 
         <Route path="/solitaire" element={<SolitairePage />} /> {/* /solitaire url is solitaire*/} 
+        <Route path="/Leaderboard" element={<Leaderboard />} /> {/* /Leaderboard url is leaderboard*/}
       </Routes>
     </BrowserRouter>
 
