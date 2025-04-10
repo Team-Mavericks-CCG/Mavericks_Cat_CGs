aaeba33a2a7615755040915fd5412400c3ead04e
night-0w1-afk <rgodoy@mail.bradley.edu>
2025-04-09 20:47:57 -0500

    wip

--- frontend/src/homePage.tsx
+++ frontend/src/homePage.tsx
@@ -18,23 +18,7 @@ import CloseIcon from "@mui/icons-material/Close";
 import { styled } from "@mui/material/styles";
 import AppTheme from "./shared-theme/AppTheme";
 
-const Card = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
-  minHeight: "75%",
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: "auto",
-  maxWidth: "650px",
-  boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-  ...theme.applyStyles("dark", {
-    boxShadow:
-      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
-  }),
-}));
+
 
 const HomePageContainer = styled(Stack)(({ theme }) => ({
   height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
@@ -268,13 +252,12 @@ function HomePage() {
   return (
     <AppTheme>
       <HomePageContainer direction="column" justifyContent="space-between">
-        <Card sx={{ flex: 1 }}>
           <Typography component="h1" variant="h4" sx={{ width: "100%" }}>
             Welcome to Mavericks Cat Card Games!
           </Typography>
           <p>Choose a game to play:</p>
           <div className="button-container">
-            <div className="button-row">
+            <div className="button-row1">
               <button className="game-button" onClick={() => setOpenSolitaire(true)}>
                 <img src="/src/images/Solitaire.png" alt="Solitaire" className="game-icon" />
                 <span className="game-label">Solitaire</span>
@@ -286,7 +269,6 @@ function HomePage() {
               <button onClick={() => setOpenBlackjack(true)}>BlackJack</button>
             </div>
           </div>
-        </Card>
       </HomePageContainer>
 
       <GameDialog
