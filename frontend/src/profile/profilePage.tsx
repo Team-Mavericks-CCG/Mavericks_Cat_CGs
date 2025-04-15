7aef0dfd1911ed6ebbaadb678439ce41d2d1bbeb
alsnyder <alsnyder@mail.bradley.edu>
2025-04-14 21:47:18 -0500

    card size fix

--- frontend/src/profile/profilePage.tsx
+++ frontend/src/profile/profilePage.tsx
@@ -27,13 +27,19 @@ const Card = styled(MuiCard)(({ theme }) => ({
   flexDirection: "column",
   alignSelf: "center",
   width: "100%",
-  padding: theme.spacing(2),
-  gap: theme.spacing(1),
+  minHeight: "75%",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
   margin: "auto",
-  boxShadow: theme.shadows[1],
   [theme.breakpoints.up("sm")]: {
-    width: "700px",
+    maxWidth: "650px",
   },
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
 }));
 
 const ProfileHeader = styled(Box)(({ theme }) => ({
